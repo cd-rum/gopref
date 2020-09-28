2b47a93e9e7c5330fcde3927e4616db132568cc3
ghost@mono <mono@endmail.xyz>
2020-09-28 14:51:58 +1000

    undo unsync

--- main.go
+++ main.go
@@ -78,7 +78,7 @@ producers:
   - name: "sync_producer"
     exchange: "gopref_env"
     routing_key: "key"
-    sync: false
+    sync: true
     options:
       content_type: "text/plain"
       delivery_mode: 2
