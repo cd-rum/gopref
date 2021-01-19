43384442df0550590f1fe9bf075f55116e5eea07
cd-rum <null@endmail.xyz>
2021-01-20 12:26:16 +1300

    ok

--- restart.sh
+++ restart.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+export UID=${UID}
+export GID=${GID}
+
 sudo docker-compose down --rmi all -v && \
   sudo docker-compose build --no-cache && \
   sudo UID=${UID} GID=${GID} docker-compose up -d --force-recreate
