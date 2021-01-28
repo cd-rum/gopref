ced3239ecfea33bf76a35d622d993ca6f4cffd8f
cd-rum <null@endmail.xyz>
2021-01-28 14:22:34 +1300

    update build inst

--- production-restart.sh
+++ production-restart.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-sudo docker-compose down --rmi all -v && \
+sudo docker container rm $(sudo docker container ls –aq) && \
+	sudo docker rmi -f $(sudo docker images -a -q) && \
+	sudo docker-compose down --rmi all -v && \
   sudo docker-compose build --no-cache && \
   sudo docker-compose -f production-docker-compose.yml up -d --force-recreate
