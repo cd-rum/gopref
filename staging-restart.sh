92b38e27d5390b136de073b00a7aeda527ccc938
cd-rum <null@endmail.xyz>
2021-01-28 14:28:02 +1300

    alright

--- staging-restart.sh
+++ staging-restart.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-sudo docker container rm $(sudo docker container ls –aq) && \
-	sudo docker rmi -f $(sudo docker images -a -q) && \
-	sudo docker-compose down --rmi all -v && \
+sudo docker-compose down --rmi all -v && \
   sudo docker-compose build --no-cache && \
   sudo docker-compose -f staging-docker-compose.yml up -d --force-recreate
